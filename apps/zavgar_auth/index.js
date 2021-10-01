5600228209ff40b678e0d399708975b689764b36
Царь всея мира <50667072+adideas@users.noreply.github.com>
2021-10-01 12:10:08 +0300

    Update index.js

--- apps/zavgar_auth/index.js
+++ apps/zavgar_auth/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import router from './router'
 import '@/zavgar_auth/src/lang'
-import App from '@/zavgar_auth/src/app.vue'
+import App from '@/zavgar_auth/src/App.vue'
 import 'wialonjs'
 
 var sess = window.wialon.core.Session.getInstance()
